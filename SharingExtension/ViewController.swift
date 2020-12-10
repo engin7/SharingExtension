270fbbd7bb5cba97d270483220cc242734a31824
Engin <kuk.engin@gmail.com>
2020-12-10 17:54:46 +0300

    tableView set

--- SharingExtension/ViewController.swift
+++ SharingExtension/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    var importedElements: [Any] = ["lol"]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        importedElements.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        // text or image
+//        cell.layer.contents = .cgImage
+        cell.textLabel?.text = importedElements[indexPath.row] as? String 
+        return cell
+    }
+    
+    
+  
+     
 
 }
 
